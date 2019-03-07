6016c5cc2dd558ca6486c02445f8ab183fee3832
chdjawe9 <48323720+chdjawe9@users.noreply.github.com>
2019-03-07 15:17:12 +0100

    Add files via upload

---
+++ autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "{DB_NAME}",
+  "dbuser"        => "{DB_USER}",
+  "dbpass"        => "{DB_PASSWORD}",
+  "dbhost"        => "{DB_HOST}",
+  "dbtableprefix" => "oc_",
+  "adminlogin"    => "{ADMIN_NAME}",
+  "adminpass"     => "{ADMIN_PASSWORD}",
+  "directory"     => "/var/www/webroot/ROOT/data",
+);
