c90d85a9812bf43641b51b5bc0ec09f1d5df7e80
chdjawe9 <48323720+chdjawe9@users.noreply.github.com>
2019-03-08 11:05:05 +0100

    Add files via upload

--- s3.config.php
+++ s3.config.php
@@ -5,16 +5,15 @@ $CONFIG = array (
     'class' => '\\OC\\Files\\ObjectStore\\S3',
     'arguments' => 
     array (
-      'bucket' => 'S3BUCKET',
+      'bucket' => '{S3BUCKET}',
       'autocreate' => true,
-      'key' => 'S3KEY',
-      'secret' => 'S3SECRET',
-      'hostname' => 'S3HOSTNAME',
-      'port' => S3PORT,
+      'key' => '{S3KEY}',
+      'secret' => '{S3SECRET}',
+      'hostname' => '{S3HOSTNAME}',
+      'port' => {S3PORT},
       'use_ssl' => true,
-      'region' => 'S3REGION',
+      'region' => '{S3REGION}',
       'use_path_style' => true,
     ),
 ),
 );
-
